b02127d3809937add54ea91c6d521a7afe172c29
Marjorie Taboada Oliveira <122323504+MarjorieTaboada@users.noreply.github.com>
2024-01-08 16:47:53 -0300

    plot2.R

---
+++ plot2.R
@@ -0,0 +1,20 @@
+# Reading the data
+data_energy <- read.table("/household_power_consumption.txt", stringsAsFactors = FALSE, header = TRUE, sep =";"  )
+
+# Changing the class of each column
+data_energy <- data_energy %>%
+  mutate(across(Date, as.Date, format="%d/%m/%Y"),
+         across(Time, ~format(as.POSIXct(., format="%H:%M:%S"), format="%H:%M:%S")),
+         across(c(Global_active_power, Global_reactive_power, Voltage, Global_intensity, Sub_metering_1, Sub_metering_2, Sub_metering_3), as.numeric))
+
+# Subsetting data from 2007-02-01 and 2007-02-02
+subsetdata <- subset(data_energy, Date == "2007-02-01" | Date =="2007-02-02")
+
+## Create column in table with date and time merged together
+timedate <- strptime(paste(data_energy$Date, data_energy$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+data_energy <- cbind(data_energy, timedate)
+        
+# Plotting the first histogram
+png("plot2.png", width=480, height=480)
+        with(subsetdata, plot(timedate, Global_active_power, type="l", xlab="Day", ylab="Global Active Power (kilowatts)"))
+        dev.off()
